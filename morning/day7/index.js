9cc8d47f9633f8eef94bcd83421b323b8121e7cf
Sonu Kumar <thesonukumar357@gmail.com>
2024-12-03 16:24:37 +0530

    added

---
+++ morning/day7/index.js
@@ -0,0 +1,12 @@
+let j = 0;
+do {
+    console.log(`This is do while running ${j} times`)
+    j++;
+} while (j < 11);
+
+
+https://wordpress.org/themes/
+
+https://themeforest.net/
+
+https://elements.envato.com/
